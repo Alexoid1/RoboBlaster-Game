b2a6e0ad5ad6c591def7f9192ccebb050199d445
Alexoid1 <alex_zam@hotmail.es>
2020-11-19 20:29:03 -0500

    setup phaser

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import Phaser from 'phaser';
+import sky from './assets/sky.png'
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+
+	this.load.image('sky', sky);
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+}
+
+function update ()
+{
+}
